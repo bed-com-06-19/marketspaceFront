eff7b7c5d9b6f88e51957e6b1deb86b098b19580
blesssingskelvinsaka@gmail.com <blessingskelvinsaka@gmail.com>
2024-01-15 15:22:26 +0200

    relatedproducts component

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+    <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box-fade">Reviews (122)</div>
+    </div>
+    <div className='descriptionbox-description'>
+        <p>   Welcome to MarketSpace, your premier destination for an unparalleled online shopping experience. At 
+              MarketSpace, we redefine the way you shop, offering an extensive range of high-quality products, seamless navigation, and exceptional customer service. 
+              Shop with confidence.
+              We prioritize the security of your transactions, implementing robust measures to safeguard your personal information.
+              We understand the excitement of receiving your purchases promptly.
+              Benefit from our efficient delivery services that bring your order straight to your doorstep.
+              Browse through a vast array of products, carefully curated to meet your needs. From fashion-forward apparel to state-of-the-art gadgets, we have it all.</p>
+              <p>
+              Discover a world of possibilities as you explore our diverse catalog featuring the latest trends in fashion, cutting-edge electronics, stylish home decor, and much more.
+               We curate a collection of premium brands and unique finds to cater to every taste and lifestyle.
+              </p>
+    </div>
+
+    </div>
+  )
+}
+
+export default DescriptionBox
