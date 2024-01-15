a12d24c3a13311cdd1814833007cf5b4ebe3dc83
blesssingskelvinsaka@gmail.com <blessingskelvinsaka@gmail.com>
2024-01-15 09:05:20 +0200

    breadcrum component & product page linking with item

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,14 +1,12 @@
 // Item.jsx
 import React from 'react';
 import './Item.css';
+import { Link } from 'react-router-dom';
 
 const Item = (props) => {
-  console.log('New Price:', props.new_price);
-  console.log('Old Price:', props.old_price);
-
   return (
     <div className='item'>
-      <img src={props.image} alt="" />
+     <Link to={`/product/${props.id}`}><img src={props.image} alt="" /></Link>
       <p>{props.name}</p>
       <div className="item-prices">
       <div className="item-price-new">
